b18900d30d3b591e9ddbb0b24fb18f2bfc937f28
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-04-16 19:52:27 -0600

    api rest agregar productos

---
+++ controllers/crearProducto.js
@@ -0,0 +1,23 @@
+import { productos } from "../servicios/service-productos.js";
+try {
+  const form = document.querySelector("[data-agregar]");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const url = document.querySelector("[data-url]").value;
+    const categoria = document.querySelector("[data-categoria]").value;
+    const producto = document.querySelector("[data-producto]").value;
+    const precio = document.querySelector("[data-precio]").value;
+    const descripcion = document.querySelector("[data-descripcion]").value;
+
+    productos
+      .crearProducto(producto, url, precio, descripcion, categoria)
+      .then(() => {
+        alert("agregado");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+} catch (error) {
+  console.log(error);
+}
