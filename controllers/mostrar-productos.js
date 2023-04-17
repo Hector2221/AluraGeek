10c83e0858846246403c10bf4eba41f6381fc5da
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-04-16 22:09:24 -0600

    codigo descomentado

--- controllers/mostrar-productos.js
+++ controllers/mostrar-productos.js
@@ -41,71 +41,71 @@ try {
   console.log(error);
 }
 
-// // // categoria starwars
-// try {
-//   const data = await productos.api();
-//   const cards = document.querySelector("[date-starwars]");
-//   const categoria = "star wars";
+// categoria starwars
+try {
+  const data = await productos.api();
+  const cards = document.querySelector("[date-starwars]");
+  const categoria = "star wars";
 
-//   const productosFiltrados = data.filter(
-//     (producto) => producto.category === categoria
-//   );
+  const productosFiltrados = data.filter(
+    (producto) => producto.category === categoria
+  );
 
-//   productosFiltrados.forEach((producto) => {
-//     const content = `<div class="card">
-//         <img class="card__img" src=${producto.imageURL} alt="" />
-//         <p class="card__title">${producto.name}</p>
-//         <p class="card__price">${producto.price}</p>
-//         <a href="" class="card__ver">Ver producto</a>
-//       </div>`;
-//     cards.innerHTML += content;
-//   });
-// } catch (error) {
-//   console.log(error);
-// }
+  productosFiltrados.forEach((producto) => {
+    const content = `<div class="card">
+        <img class="card__img" src=${producto.imageURL} alt="" />
+        <p class="card__title">${producto.name}</p>
+        <p class="card__price">${producto.price}</p>
+        <a href="" class="card__ver">Ver producto</a>
+      </div>`;
+    cards.innerHTML += content;
+  });
+} catch (error) {
+  console.log(error);
+}
 
-// // categoria consolas
-// try {
-//   const data = await productos.api();
-//   const cards = document.querySelector("[date-consolas]");
-//   const categoria = "consolas";
+// categoria consolas
+try {
+  const data = await productos.api();
+  const cards = document.querySelector("[date-consolas]");
+  const categoria = "consolas";
 
-//   const productosFiltrados = data.filter(
-//     (producto) => producto.category === categoria
-//   );
+  const productosFiltrados = data.filter(
+    (producto) => producto.category === categoria
+  );
 
-//   productosFiltrados.forEach((producto) => {
-//     const content = `<div class="card">
-//               <img class="card__img" src=${producto.imageURL} alt="" />
-//               <p class="card__title">${producto.name}</p>
-//               <p class="card__price">${producto.price}</p>
-//               <a href="" class="card__ver">Ver producto</a>
-//             </div>`;
-//     cards.innerHTML += content;
-//   });
-// } catch (error) {
-//   console.log(error);
-// }
+  productosFiltrados.forEach((producto) => {
+    const content = `<div class="card">
+              <img class="card__img" src=${producto.imageURL} alt="" />
+              <p class="card__title">${producto.name}</p>
+              <p class="card__price">${producto.price}</p>
+              <a href="" class="card__ver">Ver producto</a>
+            </div>`;
+    cards.innerHTML += content;
+  });
+} catch (error) {
+  console.log(error);
+}
 
-// // diversos consolas
-// try {
-//   const data = await productos.api();
-//   const cards = document.querySelector("[date-diversos]");
-//   const categoria = "diversos";
+// diversos consolas
+try {
+  const data = await productos.api();
+  const cards = document.querySelector("[date-diversos]");
+  const categoria = "diversos";
 
-//   const productosFiltrados = data.filter(
-//     (producto) => producto.category === categoria
-//   );
+  const productosFiltrados = data.filter(
+    (producto) => producto.category === categoria
+  );
 
-//   productosFiltrados.forEach((producto) => {
-//     const content = `<div class="card">
-//                 <img class="card__img" src=${producto.imageURL} alt="" />
-//                 <p class="card__title">${producto.name}</p>
-//                 <p class="card__price">${producto.price}</p>
-//                 <a href="" class="card__ver">Ver producto</a>
-//               </div>`;
-//     cards.innerHTML += content;
-//   });
-// } catch (error) {
-//   console.log(error);
-// }
+  productosFiltrados.forEach((producto) => {
+    const content = `<div class="card">
+                <img class="card__img" src=${producto.imageURL} alt="" />
+                <p class="card__title">${producto.name}</p>
+                <p class="card__price">${producto.price}</p>
+                <a href="" class="card__ver">Ver producto</a>
+              </div>`;
+    cards.innerHTML += content;
+  });
+} catch (error) {
+  console.log(error);
+}
